cda2cd962d0e9133646f4f508d9a533ac080b113
Rafael Silveira <64478636+rafasilveira@users.noreply.github.com>
2023-12-01 10:40:13 -0300

    01: part one

---
+++ day01/1-calibration-values.py
@@ -0,0 +1,24 @@
+import re
+
+def solution(file_path = 'input/puzzle.txt'):
+
+  file = open(file_path, 'r')
+
+  sum_calibration_values = 0
+
+  for line in file:
+    digits = re.findall(r'\d', line)   
+    calibration_value = f"{digits[0]}{digits[-1]}"
+    print(f"original line: {line.strip()}")
+    print(f"digits: {digits}")
+    print(f"calibration value: {calibration_value}\n")
+    sum_calibration_values += int(calibration_value)
+  
+  file.close()
+
+  print(f"sum of calibration values: {sum_calibration_values}")
+
+
+
+if __name__ == '__main__':
+  solution()
