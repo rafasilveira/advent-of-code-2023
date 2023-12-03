15b5a5b1f7ab2b114a6a063970573acee746b12b
Rafael Silveira <64478636+rafasilveira@users.noreply.github.com>
2023-12-03 09:37:42 -0300

    Update boilerplate

---
+++ _boilerplate/1-boilerplate.py
@@ -0,0 +1,8 @@
+def solution(file_path="input/sample.txt"):
+    with open(file_path, "r") as file:
+        for line in file:
+            print(line)
+
+
+if __name__ == "__main__":
+    solution()
