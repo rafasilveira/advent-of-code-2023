5eb53c25c7ec326deaa2125fa7cb82e654c245eb
Rafael Silveira <64478636+rafasilveira@users.noreply.github.com>
2023-12-07 11:22:07 -0300

    06: part two

--- day06/2-boats-boats-boats.py
+++ day06/2-boats-boats-boats.py
@@ -4,33 +4,19 @@ def total_distance(time_held: int, total_time: int) -> int:
 
 def solution(file_path="input/puzzle.txt"):
 
-    races = []
     input_data = open(file_path, 'r').read().splitlines()
     input_data = [d.split(': ')[1].strip().split() for d in input_data]
-    [times, distances] = input_data
-    times = [int(t) for t in times]
-    distances = [int(d) for d in distances]
+    [time, goal] = input_data
+    time = int(''.join(time))
+    goal = int(''.join(goal))
 
-    for i in range(len(times)):
-        races.append((times[i], distances[i]))
-        
-    margin_of_error = []
-    for race in races:
-        (time, goal) = race
-        ways_to_win = 0
-        for t in range(time):
-            d = total_distance(t, time)
-            if d > goal:
-                ways_to_win += 1
-        margin_of_error.append(ways_to_win)
-    mult = 1
-    for me in margin_of_error:
-        mult *= me
+    ways_to_win = 0
+    for t in range(time):
+        d = total_distance(t, time)
+        if d > goal:
+            ways_to_win += 1
             
-    print(f"margin: {margin_of_error}")
-    print(f"multiplication: {mult}")
-    
-
+    print(f"margin: {ways_to_win}")
     
 
 if __name__ == "__main__":
