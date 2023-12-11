68f6f2b51a9abbe4b0e212e0fa099d055b173859
Rafael Silveira <64478636+rafasilveira@users.noreply.github.com>
2023-12-11 15:05:26 -0300

    09: histories

---
+++ day09/1-histories.py
@@ -0,0 +1,41 @@
+from collections import Counter
+
+
+def generate_sequences(history, sequences=[]):
+    next_list = []
+    updated_sequences = sequences
+    for i in range(1, len(history)):
+        current = history[i]
+        prev = history[i - 1]
+        next_list.append(current - prev)
+    counter = Counter(next_list)
+    updated_sequences.append(next_list)
+    if counter.get(0) and len(counter.items()) == 1:
+        return updated_sequences
+    else:
+        return generate_sequences(next_list, updated_sequences)
+
+
+def solution(file_path="input/puzzle.txt"):
+    histories = []
+    predicted_values = []
+    with open(file_path, "r") as file:
+        for line in file:
+            histories.append([int(x) for x in line.split()])
+
+    for history in histories:
+        sequences = generate_sequences(history, [history])
+        sequences.reverse()
+        for i in range(len(sequences) - 1):
+            current_sequence = sequences[i]
+            next_sequence = sequences[i + 1]
+            sequences[i + 1].append(next_sequence[-1] + current_sequence[-1])
+
+        predicted_value = sequences[-1][-1]
+        predicted_values.append(predicted_value)
+
+    print(f"sum: {sum(predicted_values)}")
+
+
+if __name__ == "__main__":
+    solution()
